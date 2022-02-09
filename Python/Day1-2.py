2d35746d0907ddbab61b9a40dd57312e78eb4f1c
Eliadad <eliasashtonsaunders@gmail.com>
2022-02-08 17:39:42 -0700

    Day 1 Assignment 2 completed

---
+++ Python/Day1-2.py
@@ -0,0 +1,20 @@
+#1
+print(50+50)
+print(100-10)
+
+#2
+print(30+6, 6^6, 6**6, 6+6+6+6+6+6)
+
+#3
+print("Hello World : 10")
+print("Hello World : 10")
+
+#4
+P = 800000
+R = 6
+I = R / 100 / 12
+L = 103
+
+payment = (P*I*(1+I)**L)//((1+I)**L - 1)
+
+print(payment)
