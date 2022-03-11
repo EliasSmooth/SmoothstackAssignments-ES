0ac0dfcb4535138d51d1e9b6a0a7c41b15e8c38d
Eliadad <eliasashtonsaunders@gmail.com>
2022-03-11 15:48:01 -0700

    unittesting implemented in Flask Project

---
+++ Flask Project/test_app.py
@@ -0,0 +1,84 @@
+import email
+from urllib import response
+from flask import Flask
+import sqlalchemy
+from website import create_app
+import unittest
+from website import create_app, db
+
+app = create_app()
+
+class FlaskTestCase(unittest.TestCase):
+    def test_index_status(self):
+        tester = app.test_client(self)
+        response = tester.get('/login')
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_index_content(self):
+        tester = app.test_client(self)
+        response = tester.get('/login')
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+
+    def test_signup_status(self):
+        tester = app.test_client(self)
+        response = tester.get('/sign-up')
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_signup_content(self):
+        tester = app.test_client(self)
+        response = tester.get('/sign-up')
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+
+    def test_about_status(self):
+        tester = app.test_client(self)
+        response = tester.get('/about')
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_about_content(self): 
+        tester = app.test_client(self)
+        response = tester.get('/about')
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+
+    def test_login_post(self):
+        tester = app.test_client(self)
+        response = tester.post('/login', data=dict(email='eliasashtonsaunders@gmail.com', password="Es7585**"), follow_redirects=True)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+    
+    def test_logout(self):
+        tester = app.test_client(self)
+        tester.post('/login', data=dict(email='eliasashtonsaunders@gmail.com', password="Es7585**"), follow_redirects=True)
+        response = tester.get('/logout', follow_redirects=True)
+        print(response.status_code)
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_signup_content(self):
+        tester = app.test_client(self)
+        response = tester.get('/sign-up')
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+    
+    def test_signup_status(self):
+        tester = app.test_client(self)
+        response = tester.get('/sign-up')
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+    
+    def test_signup_form(self):
+        tester = app.test_client(self)
+        response = tester.post('/sign-up', content_type="multipart/form-data", data=dict(email="weewoo@gmail.com", username="weewoo", passwordOne="weewoo2", passwordTwo="weewoo2"))
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
+
+    def test_mainredirect(self):
+        tester = app.test_client(self)
+        response = tester.get('/', follow_redirects=True)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+
+        
+
+if __name__ == '__main__':
+    unittest.main()
--- Flask Project/website/auth.py
+++ Flask Project/website/auth.py
@@ -1,5 +1,5 @@
 from urllib import request
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, make_response
 from . import db
 from .models import User
 from flask_login import login_user, logout_user, login_required, current_user
